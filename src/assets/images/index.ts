146e49d7de54ee33b337ac953e266b2b43696432
oghenenyerhovwo-e <oghenenyerhovwoemakuneyi@gmail.com>
2025-05-01 14:45:05 +0100

    Project setup
    Added layout styles, added Mullish and Didot fonts, added images from figma file

---
+++ src/assets/images/index.ts
@@ -0,0 +1,40 @@
+import profilePic from "./profilePic.png"
+import raftingPic from "./raftingPic.png"
+import natureWalkPic from "./natureWalkPic.png"
+import bikeRidingPic from "./bikeRidingPic.png"
+import bungeeJumping from "./bungeeJumping.png"
+import wineTastingPic from "./wineTastingPic.png"
+import coffeeTastingPic from "./coffeeTastingPic.png"
+import farmVisitPic from "./farmVisitPic.png"
+import campingPic from "./campingPic.png"
+import kiberaTourPic from "./kiberaTourPic.png"
+import derekDumPic from "./derekDumPic.png"
+import derekDumPic2 from "./derekDumPic2.png"
+import mopesaLogo from "./mopesaLogo.png"
+import discoverLogo from "./discoverLogo.png"
+import visaLogo from "./visaLogo.png"
+import paypalLogo from "./paypalLogo.png"
+import masterCardLogo from "./masterCardLogo.png"
+import usaLogo from "./usaLogo.png"
+
+
+export {
+    profilePic,
+    raftingPic,
+    natureWalkPic,
+    bikeRidingPic,
+    bungeeJumping,
+    wineTastingPic,
+    coffeeTastingPic,
+    farmVisitPic,
+    campingPic,
+    kiberaTourPic,
+    derekDumPic,
+    derekDumPic2,
+    mopesaLogo,
+    discoverLogo,
+    visaLogo,
+    paypalLogo,
+    masterCardLogo,
+    usaLogo,
+}
